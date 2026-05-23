4548114267ee7e4c2c4f21f86b9893a9b0c96516
n0ticeS <yaroslav.antonenko.05@gmail.com>
2026-05-23 19:40:20 +0300

    -

---
+++ src/main/java/com/project/appliances/AppliancesApplication.java
@@ -0,0 +1,13 @@
+package com.project.appliances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/appliances/AppliancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appliances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
